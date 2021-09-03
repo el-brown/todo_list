82055233b64b9b7a17bc3ccad40594bab2172ed7
Elise Brown <elisemarie88@gmail.com>
2021-09-03 09:13:08 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from react;
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, name: "Learn Rails", complete: true, },
+      { id: 2, name: "Learn React", complete: false, },
+      { id: 3, name: "Graduate DPL", complete: false, },
+    ]
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
